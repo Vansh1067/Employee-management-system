18a2735edb05a708fc5d92ff95bd5414d9155004
Vansh1067 <50477887+Vansh1067@users.noreply.github.com>
2020-02-21 01:54:11 +0530

    card responsive

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,6 +1,7 @@
 var toggle=document.getElementById('toggle');
 var sideMenu=document.querySelector('aside');
 var sideMenuUl=document.querySelector('#sideMenu ul');
+var parentDiv;
 function flexmenu(){
     
     sideMenu.classList.toggle('hide');
