6a962472dd5a91888f3d9925504a32dbc34bf6cd
Vansh1067 <50477887+Vansh1067@users.noreply.github.com>
2020-02-21 17:06:00 +0530

    admin code

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,10 +1,14 @@
 var toggle=document.getElementById('toggle');
 var sideMenu=document.querySelector('aside');
 var sideMenuUl=document.querySelector('#sideMenu ul');
+var dropDown=document.querySelectorAll('.dropdown')
 var parentDiv;
 function flexmenu(){
     
     sideMenu.classList.toggle('hide');
+    dropDown.forEach((e)=>{
+        e.classList.toggle('hiden');
+    });
    
     
 
